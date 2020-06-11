c7043c37a6a0824258b456daeb50a2e0ccda0c01
SFort <github@ssf.tf>
2020-06-11 07:28:37 +0200

    update browser settings

---
+++ .waterfox/PROFILE.default/chrome/userChrome.js
@@ -0,0 +1,13 @@
+Object.defineProperty(window.screen, "availWidth", { get: function(){return 1920; }});
+Object.defineProperty(window.screen, "width", { get: function(){return 1920; }});
+
+Object.defineProperty(window.screen, "availHeight", { get: function(){return 1080; }});
+Object.defineProperty(window.screen, "height", { get: function(){return 1080; }});
+
+Object.defineProperty(window, "innerWidth", { get: function(){return 1920; }});
+Object.defineProperty(window, "innerHeight", { get: function(){return 974; }});
+
+Object.defineProperty(window, "outerWidth", { get: function(){return 1920; }});
+Object.defineProperty(window, "outerHeight", { get: function(){return 1040; }});
+
+Object.defineProperty(window, "devicePixelRatio", { get: function(){return 1; }});
